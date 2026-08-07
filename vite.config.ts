00763286d9d02db45a230743d7ddf9f2e89f6a64
baheumatthieu65-design <baheu.matthieu65@gmail.com>
2026-08-07 17:47:41 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Site-Internet-Beno-t/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
